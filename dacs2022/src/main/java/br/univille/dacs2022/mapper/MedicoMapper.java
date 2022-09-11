dbaef2baa2d1d79deab443e85833d6f6cd652fe1
Lucas Fagundes <lucasfagundes063@outlook.com>
2022-09-11 02:26:53 -0300

    API de procedimentos

--- dacs2022/src/main/java/br/univille/dacs2022/mapper/MedicoMapper.java
+++ dacs2022/src/main/java/br/univille/dacs2022/mapper/MedicoMapper.java
@@ -3,6 +3,7 @@ package br.univille.dacs2022.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import br.univille.dacs2022.dto.MedicoDTO;
 import br.univille.coredacs2022.entity.Medico;
@@ -13,6 +14,7 @@ public interface MedicoMapper {
 
     List<Medico> mapListMedicoDTO(List<MedicoDTO> medico);
 
+    @Mapping(target = "procedimentoId", ignore = true)
     MedicoDTO mapMedico(Medico medico);
 
     Medico mapMedicoDTO(MedicoDTO medico);
--- dacs2022/src/main/java/br/univille/dacs2022/mapper/PacienteMapper.java
+++ dacs2022/src/main/java/br/univille/dacs2022/mapper/PacienteMapper.java
@@ -3,6 +3,7 @@ package br.univille.dacs2022.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import br.univille.dacs2022.dto.PacienteDTO;
 import br.univille.coredacs2022.entity.Paciente;
@@ -12,7 +13,10 @@ public interface PacienteMapper {
     List<PacienteDTO> mapListPaciente(List<Paciente> paciente);
     List<Paciente> mapListPacienteDTO(List<PacienteDTO> paciente);
 
+    @Mapping(target = "planoId", ignore = true)
+    @Mapping(target = "cidadeId", ignore = true)
     PacienteDTO mapPaciente(Paciente paciente);
+    
     Paciente mapPacienteDTO(PacienteDTO paciente);
 }
  
